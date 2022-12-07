d1bf8997b10e537da8d0674efc5d4851ffc99b81
MotasemZiad <miziad2000@gmail.com>
2022-12-07 18:17:35 +0200

    update credentials

--- backend/drf_new/settings.py
+++ backend/drf_new/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=^9k!_=ka7woln^y)2+d6*!m-$%d)qltl1ajm3+ru6314zp!$a"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
